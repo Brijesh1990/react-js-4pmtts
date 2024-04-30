0904c3e680bbefbbe630ca3db8c287ebdd09a6ef
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-04-30 17:16:22 +0530

    30-04-2024 all data uploaded

---
+++ module2/basics-javascript/11-datatype.js
@@ -0,0 +1,121 @@
+/* 
+what is datatype ?
+  which type of data stored inside of variables i.e called datatypes.
+
+
+  There are some types of datatype  
+
+  1) primitive 
+   a) number 
+   b) bigInteger 
+   c) boolean 
+   d) undefined 
+   e) null 
+
+
+   2) non-primitive
+
+     a)array
+     b)string 
+     c)Symbol()
+     d)object 
+     e)nan
+*/
+
+
+
+// number  
+
+//let a=15454564556845;
+// console.log(a);  //typeof() this is an method to check datatypes of variables
+//console.log(typeof(a));  //typeof() this is an method to check datatypes of variables
+
+
+//bigInteger
+// let num=1545415111614313613213212211564651112165416131111111131113113;
+// console.log(typeof(num));
+
+// let num=154541511n;
+// console.log(typeof(num));
+
+
+// boolean  true or false 
+// let a=true;
+// console.log(typeof(a));
+
+// let a=false;
+// console.log(typeof(a));
+
+// let a;
+// console.log(a);
+
+// let a;
+// console.log(typeof(a));
+
+
+// let a=null; 
+// console.log(a);
+
+// let a=null; 
+// console.log(typeof(a));
+
+// let a=null; 
+// console.log(typeof(a));
+
+
+// let employee=["brijesh","naimish","kumar"];
+// // console.log(employee);
+// console.log(typeof(employee));
+
+//object 
+
+// let employee={
+
+//     id:1001,
+//     name:"brijesh",
+//     age:34,
+//     address:"150 feet ring road rajkot"
+// }
+
+// // console.log(employee)
+// console.log(typeof(employee))
+
+
+//string : A string is a set of characters i.e called string
+
+// let a='Brijesh kumar pandey';
+// let b="hi brijesh";
+// // console.log(b);
+// console.log(typeof(b));
+
+
+// string literals 
+
+// let a='hi my name is brijesh and 
+// my age is 34 years old 
+// i am done Mtech(IT)';
+// console.log(a);
+
+// let a="hi
+//  my name is brijesh and 
+// my age is 34 years old 
+// i am done Mtech(IT)";
+// console.log(a);
+
+
+// let a=`hi
+//  my name is brijesh and 
+// my age is 34 years old 
+// i am done M.tech(IT)`;
+// console.log(typeof(a));
+
+
+// Symbol : Symbol is accept or support all type of datatype
+
+// let a=Symbol("hi brijesh");
+// // console.log(a);
+// console.log(typeof(a));
+
+let a=Number.NaN;
+// console.log(a);
+console.log(typeof(a));
